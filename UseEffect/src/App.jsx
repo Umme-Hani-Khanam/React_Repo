2d1b1e7f13fb192df0ffe2b81f90b90fff48ce84
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2025-12-19 23:52:15 +0530

    Conditional Rendering — Status Toggle Component

---
+++ UseEffect/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import StatusToggle from './StatusToggle';
+
+const App = () => {
+  return (
+    <div>
+      <StatusToggle/>
+    </div>
+  )
+}
+export default App; 
